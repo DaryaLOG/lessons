ad9d81bb3598a02a910a6fd182f864bc1a05d9d7
DaryaLOG <dasha.logunova@mail.ru>
2021-02-16 23:26:41 +0300

    les4

--- LES1/src/Les1.java
+++ LES1/src/Les1.java
@@ -27,7 +27,66 @@ public class Les1 {
         System.out.println(Hello("Darya"));
 
         les2();//////Урок2
+
+        Employee darya = new Employee("Logunova Darya", "manager", 79111111111l, 150000, 27);
+        System.out.println("employee: \nname: " + darya.getName());
+        System.out.println("position: " + darya.getPosition());
+        System.out.println("phonnumber: " + darya.getPhonenumber());
+        System.out.println("salary: " + darya.getSalary());
+        System.out.println("age: " + darya.getAge());
+
+        Employee[] fiveemployees = new Employee[5];
+
+        fiveemployees[0] = new Employee();
+        fiveemployees[0].setName("Ivanov Ivan");
+        fiveemployees[0].setPosition("manager");
+        fiveemployees[0].setPhonenumber(79816666666l);
+        fiveemployees[0].setSalary(150000);
+        fiveemployees[0].setAge(39);
+
+        Employee Makarov = new Employee("Makarov Vladimir", "developer", 79113453458l, 190000, 43);
+        Employee Maslova = new Employee("Maslova Victoria", "accountant", 79818886548l, 80000, 46);
+        Employee Sidorov = new Employee("Sidorov Leonid", "developer", 79117893412l, 190000, 40);
+        Employee Sobolev = new Employee("Sobolev Konstantin", "manager", 79816783499l, 160000, 37);
+
+        fiveemployees[1] = Makarov;
+        fiveemployees[2] = Maslova;
+        fiveemployees[3] = Sidorov;
+        fiveemployees[4] = Sobolev;
+
+        for (int i = 0; i < fiveemployees.length; i++) {
+            Employee emp = fiveemployees[i];
+            if (emp.getAge() >= 40) {
+                System.out.println(emp.getName() + ": " + emp.getAge());
+
+
+            }
+
+        }
+        raiseSalary(fiveemployees);
+
+
+
+
     }
+
+    public static void raiseSalary (Employee[] fiveemployees ){
+        for (int i=0; i < fiveemployees.length; i++) {
+            if(fiveemployees [i].getAge()>=35) {
+
+                int salary = fiveemployees[i].getSalary();
+                fiveemployees[i].setSalary(salary+10000);
+                System.out.println(fiveemployees[i].getName() + ": " + fiveemployees[i].getSalary() + "; id: " + fiveemployees[i].getId());
+
+
+
+            }
+        }
+
+
+
+    }
+
 
     public static boolean Second(int f, int g) {
 
@@ -80,7 +139,6 @@ public class Les1 {
         System.out.println("min=" + min);
         int max = findMax(array4);
         System.out.println("max=" + max);
-
     }
     public static void replace (int [] array) { //////Задание1
         for (int i=0; i < array.length; i++) {
@@ -100,7 +158,6 @@ public class Les1 {
         for (int i=0; i < array.length; i++) {
             if (array [i] < 6) {
                 array [i] *=2;
-
             }
             System.out.print(array[i] +"\t");
         }
@@ -111,12 +168,9 @@ public class Les1 {
             int cur = array[i];
             if (cur < min) {
                 min = cur;
-
             }
-
         }
         return min;
-
     }
     public static int findMax (int [] array) {
         int max = array[0];
@@ -124,12 +178,9 @@ public class Les1 {
             int cur = array[i];
             if (cur > max) {
                 max = cur;
-
             }
-
         }
         return max;
-
     }
 
 
